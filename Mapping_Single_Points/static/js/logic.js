c6fae1a3250d4e0696f642a0adc61cf3359c2cf0
Sam Erdman <SamWise@OtterGames.local>
2020-08-09 14:17:20 -0700

    Adding updated map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,7 +5,27 @@ console.log("working");
 // The mapid will reference the id tag in our <div> element on the index.html file.
 // The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and 
 // the second is longitude (-94.5). We set the zoom level of “4” on a scale 0–18.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circular marker
+// L.circle([34.0522, -118.2437], {
+// 	radius: 100
+//  }).addTo(map);
+
+// Add circular marker using circleMarker()
+L.circleMarker([34.0522, -118.2437], {
+	radius: 300,
+	color: "black",
+	fillColor: '#ffffa1'
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 
 // Create the tile layer that will be the background of the map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
