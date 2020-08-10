af7c6cf043e9204e1a9d8fbc225c4ed1b3fbcc23
Sam Erdman <SamWise@OtterGames.local>
2020-08-10 16:45:21 -0700

    Adding updated logic.js file

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,11 +3,29 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 // The mapid will reference the id tag in our <div> element on the index.html file.
-// The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and 
-// the second is longitude (-94.5). We set the zoom level of “4” on a scale 0–18.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude and 
+// the second is longitude. We set the zoom level on a scale 0–18.
+// let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-// let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+// let line = [
+// 	[33.9416, -118.4085],
+// 	[37.6213, -122.3790]
+//   ];
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+	[33.9416, -118.4085],
+	[37.6213, -122.3790],
+	[40.7899, -111.9791],
+	[47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {color: "yellow"}).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
@@ -45,7 +63,10 @@ cityData.forEach(function(city) {
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 
 // Create the tile layer that will be the background of the map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+
+// Create the tile layer that will be the background of the map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
