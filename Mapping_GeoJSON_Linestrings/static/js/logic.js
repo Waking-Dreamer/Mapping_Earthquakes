b94d61ff8a319d2463d3589b441c18ff6f920eac
Sam Erdman <SamWise@OtterGames.local>
2020-08-12 11:53:47 -0700

    Adding updated files

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -88,7 +88,7 @@
 // }).addTo(map);
 
 // Create the tile layer that will be the background of the map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -111,7 +111,7 @@ let baseMaps = {
 let map = L.map('mapid', {
 	center: [44.0, -80.0],
 	zoom: 2,
-	layers: [light]
+	layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -119,11 +119,25 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 // Having the tileLayer() method before accessing large datasets ensures that the map gets loaded before the data is added to it.
-let TorontoData = "https://raw.githubusercontent.com/Waking-Dreamer/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+let TorontoData = "https://raw.githubusercontent.com/Waking-Dreamer/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/JSON/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+	color: "#ffffa1",
+	weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(TorontoData).then(function(data) {
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
-});
+  	// Creating a GeoJSON layer with the retrieved data.
+  	L.geoJson(data, {
+		style: myStyle,
+	  	onEachFeature: function(feature, layer) {
+			// Each airline route has a popup marker that shows the airline code and destination.
+		  	layer.bindPopup("<h3> Aitline: " + feature.properties.airline + "<h3> <hr><h3> Destination: "
+		  	+ feature.properties.dst + "/h3>");
+	  	}
+  	})
+  	.addTo(map);
+	});
